d0cfe6fe2547f58a1de8fc1c1b9a379f48875484
=Nii <=nii4tettey@gmail.com>
2022-05-31 23:55:56 +0200

    Print in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
-def uppercase(str):
+#!/usr/bin/python3
+def uppdef uppercase(str):
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
             i = chr(ord(i) - 32)
         print("{}".format(i), end="")
     print("")
+
