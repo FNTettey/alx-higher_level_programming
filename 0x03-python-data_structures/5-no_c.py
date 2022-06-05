f708ba39ee8cddc4261a11f6a1ee9b4d87b7f1e1
=Nii <=nii4tettey@gmail.com>
2022-06-06 01:32:17 +0200

    a string without Cs

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     """Removes all the Cs from a string."""
     new = ""
     for i in range(len(my_string)):
-        if my_string[i] != 'c' and  my_string[i] != 'C':
+        if my_string[i] != 'c' and my_string[i] != 'C':
             new = new + my_string[i]
     return(new)
