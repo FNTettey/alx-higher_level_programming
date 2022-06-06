e41935f3cf02c355a8cce02bab9b093cc8548b32
=Nii <=nii4tettey@gmail.com>
2022-06-06 16:56:13 +0200

    adding numbers from two tuples together

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,8 +8,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a1 = tuple_a[0]
         a2 = 0
     else:
-        a1 = 0
-        a2 = 0
+        a1, a2 = 0
 
     if len(tuple_b) >= 2:
         b1, b2 = tuple_b
@@ -17,8 +16,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         b1 = tuple_b[0]
         b2 = 0
     else:
-        b1 = 0
-        b2 = 0
+        b1, b2 = 0
     num1 = a1 + b1
     num2 = a2 + b2
     new = (num1, num2)
