69242703f1890cfd20ae410576219394a41b4c03
=Nii <=nii4tettey@gmail.com>
2022-06-08 17:13:38 +0200

    Squaring values in a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """computes the square value of all integers of a matrix."""
+    new_matrix = []
+    for i in range(len(matrix)):
+        row = []
+        new_matrix.append(row)
+        for j in range(len(matrix[i])):
+            element = matrix[i][j] ** 2
+            row.append(element)
+    return(new_matrix)
