b76b5cf14086d56e8b439a77986e7c5d09821702
=Nii <=nii4tettey@gmail.com>
2022-06-09 02:39:58 +0200

    Roman numeral converter

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,9 +3,8 @@ def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return (0)
- 
     rn = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'L': 50,
-    'C': 100, 'D': 500, 'M': 1000}
+          'C': 100, 'D': 500, 'M': 1000}
     value = 0
     for x in range(len(roman_string)):
         if x != len(roman_string) - 1:
@@ -19,8 +18,10 @@ def roman_to_int(roman_string):
             elif roman_string[x] == 'X':
                 if roman_string[x + 1] == 'C':
                     value = value - 10
+                    continue
             elif roman_string[x] == 'C':
                 if roman_string[x + 1] == 'M':
                     value = value - 100
-        value = value + rn[roman_string[x]]    
+                    continue
+        value = value + rn[roman_string[x]]
     return(value)
