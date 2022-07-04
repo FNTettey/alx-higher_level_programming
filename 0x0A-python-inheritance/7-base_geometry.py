d8cfce20efe2c351ad91f5a16ac5b509e012e042
=Nii <=nii4tettey@gmail.com>
2022-07-05 01:26:43 +0200

    value validation method

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,7 +9,7 @@ class BaseGeometry:
     def area(self):
         """raises an exception when called"""
         raise Exception("area() is not implemented")
-        
+
     def integer_validator(self, name, value):
         """Validates value"""
         if type(value) is not int:
