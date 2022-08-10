5c8c7bf286380282767e63bc415761297a1ba6a1
=Nii <=nii4tettey@gmail.com>
2022-08-11 01:58:30 +0200

    At least one genre

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- all the shows with at least one genre
+select tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
