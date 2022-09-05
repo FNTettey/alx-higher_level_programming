e8d481a8577c0402c901f38d05c8d941540b50d0
=Nii <=nii4tettey@gmail.com>
2022-09-06 01:00:44 +0200

    C is fun loop

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ if (isNaN(argsToConvert)) {
   console.log('Missing number of occurrences');
 } else {
   const x = argsToConvert;
-    for (let i = 0; i < x; i++) {
-	console.log('C is fun');
-    }
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
