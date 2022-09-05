2c5355c50210adb2d39261d898e51182d520f95c
=Nii <=nii4tettey@gmail.com>
2022-09-06 00:57:36 +0200

    C is fun loop

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argsToConvert = parseInt(process.argv[2]);
+if (isNaN(argsToConvert)) {
+  console.log('Missing number of occurrences');
+} else {
+  const x = argsToConvert;
+    for (let i = 0; i < x; i++) {
+	console.log('C is fun');
+    }
+}
